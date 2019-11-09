f85ac20caf84cddf1045b946b7e035b771d455ab
Shannon Ervin <sjervin07@gmail.com>
2019-11-09 17:19:59 -0600

    Alert for characters

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var num= ["0","1","2","3","4","5","6","7","8","9"]
-var low= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var cap= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var sym= ["!","@","#","$","%","^","&","*","=","+","?","-","_"]
+var len= [document.getElementById("length")]
+var num= ["0123456789"]
+var low= ["abcdefghijklmnopqrstuvwxyz"]
+var cap= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var sym= ["!@#$%^&*=+?-_"]
 
 var options= [num, low, cap, sym]
 
-var password = ""
 
 prompt ("How many characters do you want in your password?");
 
@@ -17,3 +17,9 @@ confirm("Click OK to include upper case letters");
 
 confirm("Click OK to include lower case letters")
 
+if (options !== "") {
+    alert("You must select at least one character type");
+
+
+        
+}
