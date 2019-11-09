6f4f5177cb313bdaf962367b68d8fe9983fdbf2f
Shannon Ervin <sjervin07@gmail.com>
2019-11-08 18:49:18 -0600

    Vars,prompts and confirms added

---
+++ script.js
@@ -0,0 +1,16 @@
+var num= ["0","1","2","3","4","5","6","7","8","9"]
+var low= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var cap= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var sym= ["!","@","#","$","%","^","&","*","=","+","?","-","_"]
+var password= ""
+
+prompt ("How many characters do you want in your password?");
+
+confirm("Click OK to include numbers");
+
+confirm("Click OK to include special characters");
+
+confirm("Click OK to include upper case letters");
+
+confirm("Click OK to include lower case letters")
+
