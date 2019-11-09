70e37adf6b92fe1b1227983abdd1121a3fe9f03d
Shannon Ervin <sjervin07@gmail.com>
2019-11-09 14:34:45 -0600

    Added vars

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var num= ["0","1","2","3","4","5","6","7","8","9"]
 var low= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var cap= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var sym= ["!","@","#","$","%","^","&","*","=","+","?","-","_"]
-var password= ""
+
+var options= [num, low, cap, sym]
+
+var password = ""
 
 prompt ("How many characters do you want in your password?");
 
