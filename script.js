e19c13657a1e94ef3d71fbb9e4305d3d3f306b4c
Shannon Ervin <sjervin07@gmail.com>
2019-11-09 19:59:14 -0600

    functioning alerts

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-var len= [document.getElementById("length")]
-var num= ["0123456789"]
-var low= ["abcdefghijklmnopqrstuvwxyz"]
-var cap= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var sym= ["!@#$%^&*=+?-_"]
-
-var options= [num, low, cap, sym]
-
-
-prompt ("How many characters do you want in your password?");
-
-confirm("Click OK to include numbers");
-
-confirm("Click OK to include special characters");
-
-confirm("Click OK to include upper case letters");
-
-confirm("Click OK to include lower case letters")
-
-if (options !== "") {
-    alert("You must select at least one character type");
+let num= ["0","1","2","3","4","5","6","7","8","9"]
+let low= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let cap= ["A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let sym= ["!","@","#","$","%","^&","*","=","+","?","-","_"]
+let mixed= []
+let password = ""
 
 
-        
+function generate() {
+
+
+let charLength = prompt("Enter a desired password length between 8 and 128 characters");
+
+if (charLength < 8) {
+    alert ("Please choose a length between 8-128.");
+    return;
 }
+
+    else if (charLength > 128) {
+        alert ("Please choose a length between 8-128.");
+        return; 
+    }
+
+mixed = [];
+let numYes = confirm ("Would you like to use numbers in your password?")
+if (numYes) {
+    mixed.push.apply(mixed, num)
+}
+
+let lowYes = confirm ("Would you like to use lower case letters in your password?")
+if (lowYes) {
+     mixed.push.apply(mixed, low)
+}
+
+let capYes = confirm ("Would you like to use upper case letters in your password?")
+if (capYes) {
+    mixed.push.apply(mixed,cap)
+}
+
+let symYes = confrim ("Would you like to use symbols in you password?")
+if (symYet) {
+    mixed.push.apply(mixed, sym)
+}
+
+}
+
+
+
