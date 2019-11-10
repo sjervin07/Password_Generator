4f9ce9b6a2544389238ab075f08508c45e15b541
Shannon Ervin <sjervin07@gmail.com>
2019-11-10 14:33:44 -0600

    display reset fix

--- script.js
+++ script.js
@@ -8,66 +8,64 @@ let password = "";
 
 
 function generate() {
+    password="";
+  //Conditions for password
+  let charLength = prompt(
+    "Enter a desired password length between 8 and 128 characters"
+  );
 
-//Conditions for password
-let charLength = prompt("Enter a desired password length between 8 and 128 characters");
-
-if (charLength < 8) {
-    alert ("Please choose a length between 8-128.");
+  if (charLength < 8) {
+    alert("Please choose a length between 8-128.");
     return;
-}
+  } else if (charLength > 128) {
+    alert("Please choose a length between 8-128.");
+    return;
+  }
 
-    else if (charLength > 128) {
-        alert ("Please choose a length between 8-128.");
-        return; 
-    }
+  mixed = [];
+  let numYes = confirm("Would you like to use numbers in your password?");
+  if (numYes) {
+    mixed.push.apply(mixed, num);
+  }
 
-mixed = [];
-let numYes = confirm ("Would you like to use numbers in your password?")
-if (numYes) {
-    mixed.push.apply(mixed, num)
-}
+  let lowYes = confirm(
+    "Would you like to use lower case letters in your password?"
+  );
+  if (lowYes) {
+    mixed.push.apply(mixed, low);
+  }
 
-let lowYes = confirm ("Would you like to use lower case letters in your password?")
-if (lowYes) {
-     mixed.push.apply(mixed, low)
-}
+  let capYes = confirm(
+    "Would you like to use upper case letters in your password?"
+  );
+  if (capYes) {
+    mixed.push.apply(mixed, cap);
+  }
 
-let capYes = confirm ("Would you like to use upper case letters in your password?")
-if (capYes) {
-    mixed.push.apply(mixed, cap)
-}
+  let symYes = confirm("Would you like to use symbols in your password?");
+  if (symYes) {
+    mixed.push.apply(mixed, sym);
+  }
 
-let symYes = confirm ("Would you like to use symbols in your password?")
-if (symYes) {
-    mixed.push.apply(mixed, sym)
-}
-
-//Creates the loop and generates password
-for (let x = 0; x < charLength; x++) {
-    
+  //Creates the loop and generates password
+  for (let x = 0; x < charLength; x++) {
     password = password + mixed[Math.floor(Math.random() * mixed.length)];
-}    
- 
+  }
 
-if (mixed.length === 0) {
+  if (mixed.length === 0) {
     alert("You must choose at least one character type.");
-}
-else{
-
-
-//Function that populates password into display area
-alert ("Your password is: " + password)
-document.getElementById("display").value = password;
-
-}
+  } else {
+    //Function that populates password into display area
+    alert("Your password is: " + password);
+    document.getElementById("display").value = password;
+  }
 }
 
 //Function that copies password to clipboard
 function copyPassword() {
-    document.getElementById("display").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard");
+  document.getElementById("display").select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard");
 }
 
 
